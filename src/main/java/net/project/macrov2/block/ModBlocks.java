5c7616591fa15c5f01bdfcfdc0cfcd53d715f063
Putcha <dan.hertz2@gmail.com>
2025-06-22 14:24:24 +0300

    small stuff

--- src/main/java/net/project/macrov2/block/ModBlocks.java
+++ src/main/java/net/project/macrov2/block/ModBlocks.java
@@ -12,6 +12,7 @@ import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.project.macrov2.Macrov2;
+import net.project.macrov2.block.custom.MagicBlock;
 
 public class ModBlocks {
     //blocks code start
@@ -25,6 +26,9 @@ public class ModBlocks {
     public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore", new ExperienceDroppingBlock(UniformIntProvider.create(3,6), AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
 
     //blocks code ends
+
+    public static final Block MAGIC_BLOCK = registerBlock("magic_block", new MagicBlock(AbstractBlock.Settings.create().strength(1f).requiresTool()));
+
 
     private static Block registerBlock(String name, Block block)
     {
@@ -49,6 +53,8 @@ public class ModBlocks {
 
             entries.add(PINK_GARNET_ORE);
             entries.add(PINK_GARNET_DEEPSLATE_ORE);
+
+            entries.add(ModBlocks.MAGIC_BLOCK);
 
             entries.add(ModBlocks.DIDDY_BLOCK);
         });
