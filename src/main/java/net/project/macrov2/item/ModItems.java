d82bd3cd3902d7ede2e60f639e0d5d326e0adfc0
Putcha <dan.hertz2@gmail.com>
2025-06-30 14:02:43 +0300

    set bonus effect

--- src/main/java/net/project/macrov2/item/ModItems.java
+++ src/main/java/net/project/macrov2/item/ModItems.java
@@ -9,6 +9,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.project.macrov2.item.custom.ChiselItem;
 import net.project.macrov2.item.custom.HammerItem;
+import net.project.macrov2.item.custom.ModArmorItem;
 
 public class ModItems {
     //items
@@ -67,7 +68,7 @@ public class ModItems {
     //second advanced item
     public static final Item PINK_GARNET_HAMMER = registerItem("pink_garnet_hammer",new HammerItem(ModToolMaterials.PINK_GARNET,new Item.Settings().attributeModifiers(PickaxeItem.createAttributeModifiers(ModToolMaterials.PINK_GARNET,7,-3.5f))));
     //armor
-    public static final Item PINK_GARNET_HELMET = registerItem("pink_garnet_helmet",new ArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,ArmorItem.Type.HELMET,new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15))));
+    public static final Item PINK_GARNET_HELMET = registerItem("pink_garnet_helmet",new ModArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,ArmorItem.Type.HELMET,new Item.Settings().maxDamage(ArmorItem.Type.HELMET.getMaxDamage(15))));
     public static final Item PINK_GARNET_CHESTPLATE = registerItem("pink_garnet_chestplate",new ArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,ArmorItem.Type.CHESTPLATE,new Item.Settings().maxDamage(ArmorItem.Type.CHESTPLATE.getMaxDamage(15))));
     public static final Item PINK_GARNET_LEGGINGS = registerItem("pink_garnet_leggings",new ArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,ArmorItem.Type.LEGGINGS,new Item.Settings().maxDamage(ArmorItem.Type.LEGGINGS.getMaxDamage(15))));
     public static final Item PINK_GARNET_BOOTS = registerItem("pink_garnet_boots",new ArmorItem(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,ArmorItem.Type.BOOTS,new Item.Settings().maxDamage(ArmorItem.Type.BOOTS.getMaxDamage(15))));
---
+++ src/main/java/net/project/macrov2/item/custom/ModArmorItem.java
@@ -0,0 +1,109 @@
+package net.project.macrov2.item.custom;
+
+import com.google.common.collect.ImmutableMap;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.world.World;
+import net.project.macrov2.item.ModArmorMaterials;
+
+import java.util.List;
+import java.util.Map;
+
+public class ModArmorItem extends ArmorItem {
+    private static final Map<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>>())
+                    .put(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,
+                            List.of(
+                                    //effects gained
+                                    new StatusEffectInstance(StatusEffects.STRENGTH, 20, 1, true, true),
+                                    new StatusEffectInstance(StatusEffects.ABSORPTION, 20, 2, true, true)
+                            )).build();
+                    /*
+                    * .put(ModArmorMaterials.PINK_GARNET_ARMOR_MATERIAL,
+                            List.of(
+                                    //effects gained
+                                    new StatusEffectInstance(StatusEffects.STRENGTH, 20, 1, false, false),
+                                    new StatusEffectInstance(StatusEffects.SPEED, 20, 1, false, false
+                                    ))).build();
+                                    * can add more armor sets
+                    *
+                    * */
+
+    public ModArmorItem(RegistryEntry<ArmorMaterial> material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        //every tick checks if it's not client and if it is not it checks if player has full armor on and if he does he give the status effects
+        if(!world.isClient()) {
+            if(entity instanceof PlayerEntity player) {
+                if(hasFullSuitOfArmorOn(player)) {
+                    //activate function if all requirements fulfilled
+                    evaluateArmorEffects(player);
+                }
+            }
+        }
+
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    private void evaluateArmorEffects(PlayerEntity player) {
+        for (Map.Entry<RegistryEntry<ArmorMaterial>, List<StatusEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
+            RegistryEntry<ArmorMaterial> mapArmorMaterial = entry.getKey();
+            List<StatusEffectInstance> mapStatusEffects = entry.getValue();
+
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                //run a function which gives the status effect
+                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffects);
+            }
+        }
+    }
+
+    private void addStatusEffectForMaterial(PlayerEntity player, RegistryEntry<ArmorMaterial> mapArmorMaterial, List<StatusEffectInstance> mapStatusEffect) {
+        boolean hasPlayerEffect = mapStatusEffect.stream().allMatch(statusEffectInstance -> player.hasStatusEffect(statusEffectInstance.getEffectType()));
+
+        if(!hasPlayerEffect)
+        {
+            //if doesnt have the effects already
+            for (StatusEffectInstance instance : mapStatusEffect) {
+                //gives status effect
+                player.addStatusEffect(new StatusEffectInstance(instance.getEffectType(),
+                        instance.getDuration(), instance.getAmplifier(), instance.isAmbient(), instance.shouldShowParticles()));
+            }
+        }
+    }
+
+    private boolean hasFullSuitOfArmorOn(PlayerEntity player) {
+        ItemStack boots = player.getInventory().getArmorStack(0);
+        ItemStack leggings = player.getInventory().getArmorStack(1);
+        ItemStack breastplate = player.getInventory().getArmorStack(2);
+        ItemStack helmet = player.getInventory().getArmorStack(3);
+
+        return !helmet.isEmpty() && !breastplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(RegistryEntry<ArmorMaterial> material, PlayerEntity player) {
+        for (ItemStack armorStack: player.getInventory().armor) {
+            if(!(armorStack.getItem() instanceof ArmorItem)) {
+                return false;
+            }
+        }
+
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmorStack(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmorStack(1).getItem());
+        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmorStack(2).getItem());
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmorStack(3).getItem());
+
+        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                leggings.getMaterial() == material && boots.getMaterial() == material;
+        //bool value true or false if all the armor is made from same materials
+    }
+}
